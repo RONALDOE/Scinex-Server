897d6e1a46ec6119b53c42a00661ca3e21782745
Ronaldo Suero Correa <est.ronaldoernesto@gmail.com>
2024-04-24 14:32:25 -0400

    Now doing serious commits

--- routes/posts/postActions.js
+++ routes/posts/postActions.js
@@ -2,65 +2,69 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../db/connection.js");
 
-
 // Ruta para guardar un post por un usuario
 router.post("/:postId/save", (req, res) => {
-    const userId = req.body.userId;
-    const postId = req.params.postId;
-    const sql = "INSERT INTO SavedPosts (userId, postId) VALUES (?, ?)";
-    db.query(sql, [userId, postId], (err, result) => {
-      if (err) {
-        console.error("Error al guardar el post:", err);
-        res.status(500).send("Error al guardar el post");
-      } else {
-        res.status(201).send("Post guardado correctamente");
-      }
-    });
-  });
-  
-  router.delete("/:postId/save/", (req, res) => {
-      const userId = req.query.userId;
-      const postId = req.params.postId;
-      const sql = "DELETE FROM Likes WHERE userId = ? AND postId = ?";
-      db.query(sql, [userId, postId], (err, result) => {
-        if (err) {
-          console.error("Error al desguardar el post:", err);
-          res.status(500).send("Error al desguardar el post");
-        } else {
-          res.status(201).send("Post desguardado correctamente");
-        }
-      });
-    });
-  
-  // Ruta para likear un post por un usuario
-  router.post("/:postId/like", (req, res) => {
-    const userId = req.body.userId;
-    const postId = req.params.postId;
-    const sql = "INSERT INTO Likes (userId, postId) VALUES (?, ?)";
-    db.query(sql, [userId, postId], (err, result) => {
-      if (err) {
-        console.error("Error al dar like al post:", err);
-        res.status(500).send("Error al dar like al post");
-      } else {
-        res.status(201).send("Like agregado al post");
-      }
-    });
-  });
+  const userId = req.body.userId;
+  const postId = req.params.postId;
 
-  
-// Ruta para deslikear un post por un usuario
+  const sql = "INSERT INTO SavedPosts (userId, postId) VALUES (?, ?)";
+  db.query(sql, [userId, postId], (err, result) => {
+    if (err) {
+      console.error("Error al guardar el post:", err);
+      res.status(500).send("Error al guardar el post");
+    } else {
+      console.log("Post guardado correctamente");
+      res.status(201).send("Post guardado correctamente");
+    }
+  });
+});
+
+// Ruta para desguardar un post por un usuario
+router.delete("/:postId/save", (req, res) => {
+  const userId = req.query.userId;
+  const postId = req.params.postId;
+
+  const sql = "DELETE FROM SavedPosts WHERE userId = ? AND postId = ?";
+  db.query(sql, [userId, postId], (err, result) => {
+    if (err) {
+      console.error("Error al desguardar el post:", err);
+      res.status(500).send("Error al desguardar el post");
+    } else {
+      res.status(200).send("Post desguardado correctamente");
+    }
+  });
+});
+
+// Ruta para likear un post por un usuario
+router.post("/:postId/like", (req, res) => {
+  const userId = req.body.userId;
+  const postId = req.params.postId;
+
+  const sql = "INSERT INTO Likes (userId, postId) VALUES (?, ?)";
+  db.query(sql, [userId, postId], (err, result) => {
+    if (err) {
+      console.error("Error al dar like al post:", err);
+      res.status(500).send("Error al dar like al post");
+    } else {
+      res.status(201).send("Like agregado al post");
+    }
+  });
+});
+
+// Ruta para deshacer like en un post por un usuario
 router.delete("/:postId/like", (req, res) => {
   const userId = req.query.userId;
   const postId = req.params.postId;
-    const sql = "DELETE FROM Likes WHERE userId = ? AND postId = ?";
-    db.query(sql, [userId, postId], (err, result) => {
-      if (err) {
-        console.error("Error al quitar like al post:", err);
-        res.status(500).send("Error al quitar like al post");
-      } else {
-        res.send("Like removido del post");
-      }
-    });
+
+  const sql = "DELETE FROM Likes WHERE userId = ? AND postId = ?";
+  db.query(sql, [userId, postId], (err, result) => {
+    if (err) {
+      console.error("Error al quitar like al post:", err);
+      res.status(500).send("Error al quitar like al post");
+    } else {
+      res.status(200).send("Like removido del post");
+    }
   });
-  
+});
+
 module.exports = router;
--- routes/posts/postsTrends.js
+++ routes/posts/postsTrends.js
@@ -7,8 +7,8 @@ router.get("/category/:category", (req, res) => {
     const userId = req.query.userId; // Obtener userId de la consulta URL
     const sql = `
       SELECT p.*, 
-             (CASE WHEN sp.userId IS NOT NULL THEN true ELSE false END) AS saved,
-             (CASE WHEN l.userId IS NOT NULL THEN true ELSE false END) AS liked
+             (CASE WHEN sp.userId IS NOT NULL THEN true ELSE false END) AS liked,
+             (CASE WHEN l.userId IS NOT NULL THEN true ELSE false END) AS saved
       FROM Posts p
       LEFT JOIN SavedPosts sp ON p.id = sp.postId AND sp.userId = ?
       LEFT JOIN Likes l ON p.id = l.postId AND l.userId = ?
@@ -24,50 +24,55 @@ router.get("/category/:category", (req, res) => {
       }
     });
   });
-  
- // Ruta para obtener los posts populares con información de interacción del usuario
-router.get("/popular", (req, res) => {
-  const userId = req.query.userId; // Obtener userId de la consulta URL
-  const sql = `
-    SELECT p.*, 
-           u.id as userId,
-           u.username,
-           u.badge,
-           IF(sp.userId IS NOT NULL, true, false) AS saved,
-           IF(l.userId IS NOT NULL, true, false) AS liked
-    FROM Posts p
-    INNER JOIN Users u ON p.userId = u.id
-    LEFT JOIN SavedPosts sp ON p.id = sp.postId AND sp.userId = ?
-    LEFT JOIN Likes l ON p.id = l.postId AND l.userId = ?
-    ORDER BY p.likes DESC
-    LIMIT 10`;
-  db.query(sql, [userId, userId], (err, results) => {
-    if (err) {
-      console.error("Error al obtener posts populares:", err);
-      res.status(500).send("Error al obtener posts populares");
-    } else {
-      // Transformar los resultados al formato deseado (IPost)
-      const formattedResults = results.map((post) => ({
-        _id: post.id.toString(),
-        title: post.title,
-        content: post.content,
-        image: post.image || null,
-        user: {
-          id: post.userId.toString(), 
-          username: post.username,
-          badge: post.badge
-        },
-        likes: post.likes,
-        createdAt: post.createdAt.toISOString(),
-        updatedAt: post.updatedAt.toISOString(),
-        liked: post.liked === 1 ? true : false,
-        saved: post.saved === 1 ? true : false
-      }));
 
-      res.json(formattedResults);
-    }
+//Si, ya se que esta al revez los if, pero juro por Dios, que si lo pongo al derecho no me funciona
+//Alguna vez has leido sobre el coco de TF2? Pues ya puedes entender mi confusión
+
+  router.get("/popular", (req, res) => {
+    const userId = req.query.userId; // Obtener userId de la consulta URL
+    const sql = `
+      SELECT p.*, 
+             u.id as userId,
+             u.username,
+             u.badge,
+             IF(l.userId IS NOT NULL, true, false) AS saved,
+             IF(sp.userId IS NOT NULL, true, false) AS liked
+      FROM Posts p
+      INNER JOIN Users u ON p.userId = u.id
+      LEFT JOIN SavedPosts sp ON p.id = sp.postId AND sp.userId = ?
+      LEFT JOIN Likes l ON p.id = l.postId AND l.userId = ?
+      ORDER BY p.likes DESC
+      LIMIT 10`;
+  
+    db.query(sql, [userId, userId], (err, results) => {
+      if (err) {
+        console.error("Error al obtener posts populares:", err);
+        res.status(500).send("Error al obtener posts populares");
+      } else {
+        // Transformar los resultados al formato deseado (IPost)
+        const formattedResults = results.map((post) => ({
+          _id: post.id.toString(),
+          title: post.title,
+          content: post.content,
+          image: post.image || null,
+          user: {
+            id: post.userId.toString(),
+            username: post.username,
+            badge: post.badge
+          },
+          likes: post.likes,
+          createdAt: post.createdAt.toISOString(),
+          updatedAt: post.updatedAt.toISOString(),
+          liked: post.liked === 1,
+          saved: post.saved === 1,
+        }));
+        
+        console.log(results[0])
+        res.json(formattedResults);
+      }
+    });
   });
-});
+  
 
 
 module.exports = router;
